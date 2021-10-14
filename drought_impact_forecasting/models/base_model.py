b78a059fc046a05919f0880c549058b3f2125518
omrazCZ <oto.mraz@gmail.com>
2021-10-14 13:01:43 +0200

    Add automatic Pytorch LR decay

--- drought_impact_forecasting/models/base_model.py
+++ drought_impact_forecasting/models/base_model.py
@@ -1,6 +1,6 @@
 from torch import nn
 import torch.optim as optim
-import torch.optim.lr_scheduler as lrs
+from torch.optim.lr_scheduler import LambdaLR
 import pytorch_lightning as pl
 from drought_impact_forecasting.losses import kl_weight, base_line_total_loss
 from .model_parts.base_model import Encoder, Discriminator_GAN, Discriminator_VAE
@@ -19,33 +19,25 @@ class Base_model(pl.LightningModule):
 
         self.Discriminator_GAN = Discriminator_GAN(self.cfg)
 
-    def forward(self):
-        pass
+    # For now just use the GAN
+    def forward(self, x):
+        self.Discriminator_GAN.forward(x)
 
     def configure_optimizers(self):
         if self.cfg["training"]["optimizer"] == "adam":
             optimizer = optim.Adam(self.parameters(), lr=self.cfg["training"]["start_learn_rate"])
+            
+            # Decay learning rate according for last (epochs - decay_point) iterations
+            lambda_all = lambda epoch: self.cfg["training"]["start_learn_rate"] if epoch <= self.cfg["training"]["decay_point"] else ((self.cfg["training"]["epochs"]-epoch)/(30-self.cfg["training"]["decay_point"]) * self.cfg["training"]["start_learn_rate"])
+            scheduler = LambdaLR(optimizer, lambda_all)
         else:
             raise ValueError("You have specified an invalid optimizer.")
 
-        return [optimizer]
+        # Pls check this works correctly with pytorch lightning
+        return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
         batch_x, batch_y = batch
-
-        # Useless code?
-        """
-        L1_criterion = nn.L1Loss()
-        KL_criterion = nn.KLDivLoss()
-        GAN_criterion = nn.CrossEntropyLoss() 
-        """
-
-
-        # TODO: Adapt adaptive learning rate to new code structure/find a pytorch technique for doing this
-
-        """curLearningRate = self.__get_learning_rate(i)
-        for g in optimizer.param_groups:
-            g['lr'] = curLearningRate"""
 
         y_preds = self(batch_x)
         loss_total = base_line_total_loss(y_preds, batch_y, self.current_epoch)
@@ -54,9 +46,3 @@ class Base_model(pl.LightningModule):
 
     def validation_step(self):
         pass
-
-    """def __get_learning_rate(self, epoch):
-        if epoch < decayPoint:
-            return startLearnRate
-        else:
-            startLearnRate * ((epoch - decayPoint) / (epochs - decayPoint))"""
