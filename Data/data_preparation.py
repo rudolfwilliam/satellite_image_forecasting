24e6f1eee1935f885fd119e53e19af9664f8b3a0
omrazCZ <oto.mraz@gmail.com>
2021-11-05 17:20:25 +0100

    Add weather data

--- Data/data_preparation.py
+++ Data/data_preparation.py
@@ -34,22 +34,6 @@ def prepare_data(training_samples, ms_cut, train_dir, test_dir):
     train = Earthnet_Dataset(train_files, ms_cut)
     test = Earthnet_Dataset(test_context_files, ms_cut, test_target_files)
     return train, test
- 
-    train_data = []
-    test_context_data = []
-    test_target_data = []
-
-    # limit number of training samples
-    for i in range(min(len(train_files), training_samples)):
-        train_data.append(np.load(train_files[i], allow_pickle=True))
- 
-    for i in range(len(test_context_files)):
-        test_context_data.append(np.load(test_context_files[i], allow_pickle=True))
-        test_target_data.append(np.load(test_context_files[i], allow_pickle=True))
-    
-    train = Earthnet_Dataset(train_data, ms_cut)
-    test = Earthnet_Dataset(test_context_data, ms_cut, test_target_data)
-    return train, test
 
 class Earthnet_Dataset(torch.utils.data.Dataset):
     def __init__(self, context_file_paths, ms_cut, target_file_paths = None):
@@ -57,8 +41,7 @@ class Earthnet_Dataset(torch.utils.data.Dataset):
             context_file_paths: list of paths of all the context files
             ms_cut: indxs for relevant mesoscale data
             target_file_paths: list of paths of all the context files
-        '''
-        '''
+
             The EarthNet dataset combines the different components of a earchnet data cube.
 
             highres dynamic (hrs) - leave as is as is (for test samples we glue together the contex & target data)
@@ -75,30 +58,64 @@ class Earthnet_Dataset(torch.utils.data.Dataset):
         '''
 
         self.context_paths = context_file_paths
-        self.target_paths = None
-        if target_file_paths is not None:
-            self.traget_paths = target_file_paths
+        self.ms_cut = ms_cut
+        self.target_paths = target_file_paths
 
+    def process_md(self, md, target_shape):
+        '''
+            Channels: Precipitation (RR), Sea pressure (PP), Mean temperature (TG), Minimum temperature (TN), Maximum temperature (TX)
+        '''
+        interval = round(md.shape[3] / target_shape[3])
 
-        
+        md_new = np.empty((tuple([md.shape[0]] + [md.shape[1]] + [md.shape[2]] + [30])))
+        # Make avg, min, max vals over 5 day intervals
+        for i in range(target_shape[3]):
+            days = [d for d in range(i*interval, i*interval + interval)]
+            for j in range(md.shape[0]):
+                for k in range(md.shape[1]):
+                    md_new[j,k,0,i] = np.mean(md[j,k,0,days])   # mean precipitation
+                    md_new[j,k,1,i] = np.mean(md[j,k,1,days])   # mean pressure
+                    md_new[j,k,2,i] = np.mean(md[j,k,2,days])   # mean temp
+                    md_new[j,k,3,i] = np.min(md[j,k,3,days])    # min temp
+                    md_new[j,k,4,i] = np.max(md[j,k,4,days])    # max temp
+
+        # Reshape to 128 x 128
+        md_reshaped = np.empty((tuple([target_shape[0]] + [target_shape[1]] + [md.shape[2]] + [md_new.shape[3]])))
+        for i in range(target_shape[0]):
+            for j in range(target_shape[1]):
+                row = round(i//(target_shape[0]/md.shape[0]))
+                col = round(j//(target_shape[1]/md.shape[1]))
+                md_reshaped[i,j,:,:] = md_new[row, col,:,:]
+
+        return md_reshaped
  
     def __len__(self):
         return len(self.context_paths)
  
     def __getitem__(self, index):
-        # load the item from data
+        # Load the item from data
         context = np.load(self.context_paths[index], allow_pickle=True)
 
+        # For test samples glue together context & target
         if self.target_paths is not None:
             target = np.load(self.target_paths[index], allow_pickle=True)
             highres_dynamic = np.nan_to_num(np.append(context['highresdynamic'], target['highresdynamic'],axis=-1), nan = 0.0)
         else:
             highres_dynamic = np.nan_to_num(context['highresdynamic'], nan = 0.0) 
+        highres_static = np.repeat(np.expand_dims(np.nan_to_num(context['highresstatic'], nan = 0.0), axis=-1), repeats=30, axis=-1)
+        # For mesoscale data cut out overlapping section of interest
+        meso_dynamic = np.nan_to_num(context['mesodynamic'], nan = 0.0)[self.ms_cut[0]:self.ms_cut[1],self.ms_cut[0]:self.ms_cut[1],:,:]
+        meso_static = np.nan_to_num(context['mesostatic'], nan = 0.0)[self.ms_cut[0]:self.ms_cut[1],self.ms_cut[0]:self.ms_cut[1],:]
 
-        highres_static = np.nan_to_num(context['highresstatic'], nan = 0.0)
-        meso_dynamic = np.nan_to_num(context['mesodynamic'], nan = 0.0)
-        meso_static = np.nan_to_num(context['mesostatic'], nan = 0.0)
- 
+        # Add up the total number of channels
+        channels = highres_dynamic.shape[2] + highres_static.shape[2] + meso_dynamic.shape[2]
+
+        all_data = np.append(highres_dynamic, highres_static,axis=-2)
+
+        meso_dynamic = self.process_md(meso_dynamic, tuple([all_data.shape[0]] + [all_data.shape[1]] + 
+                                            [meso_dynamic.shape[2]] + [all_data.shape[3]]))
+        all_data = np.append(all_data, meso_dynamic, axis=-2)
+        
         ''' Permute data so that it fits the Pytorch conv2d standard. From (w, h, c, t) to (c, w, h, t)
             w = width
             h = height
@@ -106,12 +123,10 @@ class Earthnet_Dataset(torch.utils.data.Dataset):
             t = time
         '''
         highres_dynamic = torch.Tensor(highres_dynamic).permute(2, 0, 1, 3)
+        all_data = torch.Tensor(highres_dynamic).permute(2, 0, 1, 3)
+        
 
-        # Add up the total number of channels
-        channels = context['highresdynamic'].shape[2] + context['highresstatic'].shape[2] + context['mesodynamic'].shape[2]
-
-
-        return highres_dynamic, highres_static, meso_dynamic, meso_static
+        return highres_dynamic, highres_static, meso_dynamic, meso_static, all_data
 
         hrs_shape = list(context['highresdynamic'].shape)
         # If target data is given separately add context + target dimensions
@@ -158,3 +173,6 @@ class Earthnet_Dataset(torch.utils.data.Dataset):
 
         return self.highres_dynamic[index], self.highres_static[index], self.meso_dynamic[index], \
                self.meso_static[index]
+
+
+
