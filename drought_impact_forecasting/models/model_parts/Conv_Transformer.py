b4303fa0f20b42a96d54b49f4f70da44cba0eca2
kkladny <kkladny@student.ethz.ch>
2021-12-03 20:32:20 +0100

    Enable masked query self attention

--- drought_impact_forecasting/models/model_parts/Conv_Transformer.py
+++ drought_impact_forecasting/models/model_parts/Conv_Transformer.py
@@ -39,53 +39,66 @@ class FeedForward(nn.Module):
 
 
 class ConvAttention(nn.Module):
-    def __init__(self, configs, num_hidden, enc):
+    def __init__(self, configs, num_hidden, enc, mask=False):
         super(ConvAttention, self).__init__()
         self.configs = configs
         self.enc = enc
+        self.mask = mask
         self.num_hidden = num_hidden
         # important note: shared convolution is intentional here
         if self.enc:
             self.conv1 = nn.Sequential(
-                nn.Conv2d(in_channels=self.num_hidden, out_channels=3 * self.num_hidden, kernel_size=1)
+                nn.Conv2d(in_channels=self.num_hidden, out_channels=3 * self.num_hidden, kernel_size=1, padding="same")
             )
         else:
             self.conv1 = nn.Sequential(
-                nn.Conv2d(in_channels=self.num_hidden, out_channels=2 * self.num_hidden, kernel_size=1)
+                nn.Conv2d(in_channels=self.num_hidden, out_channels=2 * self.num_hidden, kernel_size=1, padding="same")
             )
-        #TODO: (optional) make this have multiple layers
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=self.num_hidden*2, out_channels=1, kernel_size=5, padding=2)
+            nn.Conv2d(in_channels=self.num_hidden*2, out_channels=1, kernel_size=self.configs["kernel_size"], padding="same")
         )
 
     def forward(self, x, enc_out=None):
-        b, c, h, w, t = x.shape
-        qkv_setlist = []
-        if not self.enc:
-            s = enc_out.size()[-1]
-            for i in range(s):
-                qkv_setlist.append(self.conv1(enc_out[..., i]))
-        else:
-            s = t
+        b, c, h, w, s = x.shape
+        if self.enc:
+            qkv_setlist = []
+            t = s
             for i in range(t):
                 qkv_setlist.append(self.conv1(x[..., i]))
-
-        qkv_set = torch.stack(qkv_setlist, dim=-1)
-        if not self.enc:
-            K, V = torch.split(qkv_set, self.num_hidden, dim=1)
-            Q = x
-        else:
+            qkv_set = torch.stack(qkv_setlist, dim=-1)
             Q, K, V = torch.split(qkv_set, self.num_hidden, dim=1)
+        else:
+            # x correspond to the query
+            kv_setlist = []
+            t = enc_out.size()[-1]
+            for i in range(t):
+                kv_setlist.append(self.conv1(enc_out[..., i]))
+            kv_set = torch.stack(kv_setlist, dim=-1)
+            K, V = torch.split(kv_set, self.num_hidden, dim=1)
+            Q = x
 
-        K_rep = torch.stack([K] * t, dim=-1)
-        V_rep = torch.stack([V] * t, dim=-1)
-        Q_rep = torch.stack([Q] * s, dim=-1)
-        K_flip = rearrange(K_rep, 'b c h w s t -> b c h w t s')
-        Q_K = rearrange(torch.concat((Q_rep, K_flip), dim=1), 'b c h w t s -> (b t s) c h w') # no convolution across time dim!
-        extr_feat = rearrange(torch.squeeze(self.conv2(Q_K)), '(b t s) h w -> b h w s t', b=b, t=t)
-        attn_mask = F.softmax(extr_feat, dim=-2)
-        V_pre = torch.stack([torch.mul(attn_mask, V_rep[:, c, ...]) for c in range(V_rep.size()[1])], dim=1)
-        V_out = torch.sum(V_pre, dim=-2)
+        K_rep = torch.stack([K] * s, dim=-1)
+        V_rep = torch.stack([V] * s, dim=-1)
+        Q_rep = torch.stack([Q] * t, dim=-1)
+        K_flip = rearrange(K_rep, 'b c h w t s -> b c h w s t')
+        Q_K = torch.concat((Q_rep, K_flip), dim=1) 
+        if self.mask:
+            # only feed in 'previous' keys & values for computing softmax
+            V_out = []
+            for i in range(t):
+                Q_K_temp = Q_K[..., :i+1, i]
+                Q_K_temp = rearrange(Q_K_temp, 'b c h w t-> (b t) c h w') # no convolution across time dim!
+                extr_feat = rearrange(torch.squeeze(self.conv2(Q_K_temp)), '(b t) h w -> b h w t', b=b, t=i+1)
+                attn_mask = F.softmax(extr_feat, dim=-1)
+                V_pre = torch.stack([torch.mul(attn_mask, V_rep[:, c, :, :, i, :i+1]) for c in range(V_rep.size()[1])], dim=1)
+                V_out.append(torch.sum(V_pre, dim=-1))
+            V_out = torch.stack(V_out, dim=-1)
+        else:
+            Q_K = rearrange(Q_K, 'b c h w s t -> (b s t) c h w') # no convolution across time dim!
+            extr_feat = rearrange(torch.squeeze(self.conv2(Q_K)), '(b s t) h w -> b h w t s', b=b, t=t)
+            attn_mask = F.softmax(extr_feat, dim=-2)
+            V_pre = torch.stack([torch.mul(attn_mask, V_rep[:, c, ...]) for c in range(V_rep.size()[1])], dim=1)
+            V_out = torch.sum(V_pre, dim=-2)
 
         return V_out
 
@@ -109,8 +122,8 @@ class PositionalEncoding(nn.Module):
 
         sinusoid_table = [get_position_angle_vec(pos_i) for pos_i in range(t)]
         sinusoid_table = torch.stack(sinusoid_table, dim=0)
-        sinusoid_table[:, :, 0::2] = np.sin(sinusoid_table[:, :, 0::2])  # dim 2i
-        sinusoid_table[:, :, 1::2] = np.cos(sinusoid_table[:, :, 1::2])  # dim 2i+1
+        sinusoid_table[:, :, 0::2] = torch.sin(sinusoid_table[:, :, 0::2])  # even dim
+        sinusoid_table[:, :, 1::2] = torch.cos(sinusoid_table[:, :, 1::2])  # odd dim
 
         return torch.moveaxis(sinusoid_table, 0, -1)
 
@@ -148,10 +161,10 @@ class Decoder(nn.Module):
         self.num_hidden = self.configs["num_hidden"]
         self.num_non_pred_feat = self.configs["non_pred_channels"]
         for _ in range(self.configs["depth"]):
-            if self.configs["query_self_attention"]:
-                self.layers.append(nn.ModuleList([Residual(PreNorm([self.num_hidden[-1], self.configs["img_width"], self.configs["img_width"]],
-                                     ConvAttention(self.configs, self.num_hidden[-1], enc=True)))]))
             self.layers.append(nn.ModuleList([
+                # (masked) query self-attention
+                Residual(PreNorm([self.num_hidden[-1], self.configs["img_width"], self.configs["img_width"]],
+                                     ConvAttention(self.configs, self.num_hidden[-1], enc=True, mask=True))),
                 # convolutional attention
                 Residual(PreNorm([self.num_hidden[-1], self.configs["img_width"], self.configs["img_width"]],
                                  ConvAttention(self.configs, self.num_hidden[-1], enc=False))),
@@ -167,50 +180,12 @@ class Decoder(nn.Module):
                 x = attn(queries, enc_out=enc_out)
                 x = ff(x)
         else:
-            for attn, ff in self.layers:
+            for dec, attn, ff in self.layers:
+                x = dec(queries)
                 x = attn(queries, enc_out=enc_out)
                 x = ff(x)
 
         return x
-
-
-class Feature_Generator(nn.Module):
-    def __init__(self, configs):
-        super(Feature_Generator, self).__init__()
-        self.configs = configs
-        self.num_hidden = self.configs["num_hidden"]
-        self.conv1 = nn.Conv2d(in_channels=11,
-                               out_channels=self.num_hidden[0],
-                               kernel_size=self.configs["kernel_size"],
-                               stride=1,
-                               padding="same")
-        self.conv2 = nn.Conv2d(in_channels=self.num_hidden[0],
-                               out_channels=self.num_hidden[1],
-                               kernel_size=self.configs["kernel_size"],
-                               stride=1,
-                               padding="same")
-        self.conv3 = nn.Conv2d(in_channels=self.num_hidden[1],
-                               out_channels=self.num_hidden[2],
-                               kernel_size=self.configs["kernel_size"],
-                               stride=1,
-                               padding="same")
-        self.conv4 = nn.Conv2d(in_channels=self.num_hidden[2],
-                               out_channels=self.num_hidden[3],
-                               kernel_size=self.configs["kernel_size"],
-                               stride=1,
-                               padding="same")
-        self.bn1 = nn.BatchNorm2d(self.num_hidden[0])
-        self.bn2 = nn.BatchNorm2d(self.num_hidden[1])
-        self.bn3 = nn.BatchNorm2d(self.num_hidden[2])
-        self.bn4 = nn.BatchNorm2d(self.num_hidden[3])
-
-    def forward(self, x):
-        out = F.leaky_relu(self.bn1(self.conv1(x)), negative_slope=0.01, inplace=False)
-        out = F.leaky_relu(self.bn2(self.conv2(out)), negative_slope=0.01, inplace=False)
-        out = F.leaky_relu(self.bn3(self.conv3(out)), negative_slope=0.01, inplace=False)
-        out = F.leaky_relu(self.bn4(self.conv4(out)), negative_slope=0.01, inplace=False)
-        return out
-
 
 class Conv_Transformer(nn.Module):
     def __init__(self, configs):
@@ -220,8 +195,9 @@ class Conv_Transformer(nn.Module):
         self.pos_embedding = PositionalEncoding(self.configs)
         self.Encoder = Encoder(self.configs)
         self.Decoder = Decoder(self.configs)
+        self.input_feat_gen = Conv_Block(self.configs["in_channels"], self.num_hidden[-1], num_conv_layers=configs["num_layers_input_feat"], kernel_size=configs["kernel_size"])
         # make the dimensions of the non_pred features fit
-        self.dim_fit = Conv_Block(self.configs["non_pred_channels"], self.num_hidden[-1], num_conv_layers=1, kernel_size=configs["kernel_size"])
+        self.query_feat_gen = Conv_Block(self.configs["non_pred_channels"], self.num_hidden[-1], num_conv_layers=configs["num_layers_query_feat"], kernel_size=configs["kernel_size"])
         # last predictions needs a dummy input
         self.blank = torch.stack([torch.zeros(size=(configs["batch_size"], configs["non_pred_channels"],
                                                     self.configs["img_width"], self.configs["img_width"]))], dim=-1)
@@ -232,7 +208,7 @@ class Conv_Transformer(nn.Module):
 
     def forward(self, frames, prediction_count, non_pred_feat = None):
         _, _, _, _, t = frames.size()
-        feature_map = self.feature_embedding(img=frames, configs=self.configs)
+        feature_map = self.feature_embedding(img=frames, network=self.input_feat_gen)
         enc_in = self.pos_embedding(feature_map, t)
         enc_out = self.Encoder(enc_in)
         # queries correspond to non_pred_feat
@@ -241,7 +217,7 @@ class Conv_Transformer(nn.Module):
         else:
             non_pred_feat = self.blank
 
-        dec_V = torch.stack([self.dim_fit(non_pred_feat[..., i]) for i in range(non_pred_feat.size()[-1])], dim=-1)
+        dec_V = self.feature_embedding(img=non_pred_feat, network=self.query_feat_gen)
         dec_out = self.Decoder(dec_V, enc_out)
         out_list = []
         for i in range(prediction_count):
@@ -250,8 +226,8 @@ class Conv_Transformer(nn.Module):
 
         return x
 
-    def feature_embedding(self, img, configs):
-        generator = Feature_Generator(configs).to(configs["device"])
+    def feature_embedding(self, img, network):
+        generator = network
         gen_img = []
         for i in range(img.shape[-1]):
             gen_img.append(generator(img[..., i]))
