1c57221d1e7919e3c5378b242acb51d0d4a33d8e
Marco Milanta <marco.milanta@gmail.com>
2021-12-17 14:58:55 +0100

    settabl

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -49,29 +49,4 @@ with open(os.path.join(os.getcwd(), "Data", "all_data", "test_context_data_paths
 with open(os.path.join(os.getcwd(), "Data", "all_data", "test_target_data_paths.pkl"), "wb") as fp:
     pickle.dump(test_data.target_paths, fp)
 
-# Create NDVI dataset (only using heavilly vegetated cubes)
-NDVI_dataset = Earthnet_NDVI_Dataset(training_data.paths, cfg["data"]["mesoscale_cut"], device=device, veg_threshold=cfg["data"]["veg_threshold"])
-NDVI_dataset.filter_non_vegetated()
-
-NDVI_data = NDVI_dataset.paths
-dataset_size = len(NDVI_data)
-
-train_set = random.sample(NDVI_data, floor(dataset_size * 0.75))
-rest = [x for x in NDVI_data if x not in train_set]
-val_set = random.sample(rest, floor(dataset_size * 0.01))
-if (len(val_set) == 0): # To work on small PC datasets
-    val_set.append(train_set[-1])
-    train_set = train_set[0:-1]
-test_set = [x for x in rest if x not in val_set]
-
-# To build back the datasets
-with open(os.path.join(os.getcwd(), "Data", "NDVI_data", "train_data_paths.pkl"), "wb") as fp:
-    pickle.dump(train_set, fp)
-with open(os.path.join(os.getcwd(), "Data", "NDVI_data", "val_data_paths.pkl"), "wb") as fp:
-    pickle.dump(val_set, fp)
-with open(os.path.join(os.getcwd(), "Data", "NDVI_data", "test_data_paths.pkl"), "wb") as fp:
-    pickle.dump(test_set, fp)
-
-print(len(train_set))
-print(len(val_set))
-print(len(test_set))
+print("Done")
