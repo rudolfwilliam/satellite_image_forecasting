248ca64c01f50a86e8e5f5bb1e0a9a9d02eaa14d
omrazCZ <oto.mraz@gmail.com>
2021-12-23 03:57:50 +0100

    Finish data collection & data cleaning

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -1,60 +1,108 @@
 from math import floor
 import sys
 import os
-import json
+import warnings
+from os.path import isfile, join, split
 import random
-import time
 import pickle
-
-import torch
-from torch.utils import data
-import pytorch_lightning as pl
+import glob
+import numpy as np
+from numpy import genfromtxt
+import re
 
 sys.path.append(os.getcwd())
 
-from config.config import command_line_parser
-from Data.data_preparation import Earthnet_Dataset, Earthnet_NDVI_Dataset, Earthnet_Context_Dataset, prepare_train_data, prepare_test_data
-
 import argparse
+
+training_samples = 23904
 
 parser = argparse.ArgumentParser(
         add_help=True,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-parser.add_argument('-s', '--source_folder', type=str, help='folder where data is')
+parser.add_argument('-s', '--source_folder', type=str, help='folder where the training data is')
+parser.add_argument('-tf', '--test_folder', type=str, help='folder where the test data is', default=None)
 parser.add_argument('-d', '--dest_folder', type=str,  help='folder where pickles shoud be saved')
-parser.add_argument('-td', '--training_data', type=int,  help='folder where pickles shoud be saved')
-parser.add_argument('-v1', '--val_1_data', type=int,  help='folder where pickles shoud be saved')
-parser.add_argument('-v2', '--val_2_data', type=int,  help='folder where pickles shoud be saved', default=-1)
+parser.add_argument('-td', '--training_data', type=int,  help='number of training data points')
+parser.add_argument('-v1', '--val_1_data', type=int,  help='number of validation 1 data points')
+parser.add_argument('-v2', '--val_2_data', type=int,  help='number of validation 2 data points', default=-1)
+parser.add_argument('-se', '--seed', type=int,  help='seed for train/validation split', default=1)
+parser.add_argument('-dc', '--data_cleaning', type=float,  help='level of data cleaning (-1: none, 0: cubes giving NaN, (0,1]: min score)', default=-1)
 args = parser.parse_args()
 
-train_files = []
-for path, subdirs, files in os.walk(os.getcwd() + args.source_folder):    
-    for name in files:
-        # Ignore any licence, progress, etc. files
-        if '.npz' in name:
-            train_files.append(os.join(path, name))
+# Collect all cubes in the training set
+train_files = glob.glob(os.getcwd() + args.source_folder + '/**/*.npz', recursive=True)
+train_files.sort()
+if len(train_files) != training_samples:
+    warnings.warn("Your training set is incomplete! You only have " + str(len(train_files)) + " samples instead of " + str(training_samples))
 
-train_files = random.shuffle(train_files)
+# Clean data
+if args.data_cleaning != -1:
+    baseline_scores = genfromtxt(join(os.getcwd(), "Data", "scores_last_frame.csv"), delimiter=',')
+    with open(join(os.getcwd(), "Data", "last_frame_data_paths.pkl"),'rb') as f:
+        old_train_paths = pickle.load(f)
+    old_train_paths = [path.split()[-1] for path in old_train_paths]
+
+    # Collect samples yielding NaN or below threshold
+    bad_cubes = []
+    for i in range(len(baseline_scores)):
+        if np.isnan(baseline_scores[i, -1]):
+            bad_cubes.append(split(old_train_paths[i])[-1])
+    if args.data_cleaning > 0:
+        for i in range(len(baseline_scores)):
+            if baseline_scores[i, -1] < args.data_cleaning:
+                bad_cubes.append(split(old_train_paths[i])[-1])
+    
+    # Discard bad samples
+    train_files = [f for f in train_files if split(f)[-1] not in bad_cubes]
+
+    # If we have too little samples after cleaing, decrease the training set size
+    if args.training_data + args.val_1_data + args.val_2_data > len(train_files):
+        args.training_data = len(train_files) - args.val_1_data - args.val_2_data
+
+random.seed(args.seed)
+random.shuffle(train_files)
+
 training_data = train_files[:args.training_data]
 val_1_data = train_files[args.training_data: args.training_data + args.val_1_data]
 if args.val_2_data == -1:
     val_2_data = train_files[args.training_data + args.val_1_data: -1]
 else:
     val_2_data = train_files[args.training_data + args.val_1_data:args.training_data + args.val_1_data+args.val_2_data]
-# Create dirs if needed
-if not os.path.exists(os.path.join(os.getcwd(), "Data", "all_data")):
-    os.makedirs(os.path.join(os.getcwd(), "Data", "all_data"))
-if not os.path.exists(os.path.join(os.getcwd(), "Data", "NDVI_data")):
-    os.makedirs(os.path.join(os.getcwd(), "Data", "NDVI_data"))
+# Create pickle dir if needed
+if not os.path.exists(join(os.getcwd(), "Data", args.dest_folder)):
+    os.makedirs(os.path.join(os.getcwd(), "Data", args.dest_folder))
 
 # To build back the datasets
-with open(os.path.join(os.getcwd(), "Data", "all_data", "train_data_paths.pkl"), "wb") as fp:
-    pickle.dump(training_data.paths, fp)
-with open(os.path.join(os.getcwd(), "Data", "all_data", "val_1_data_paths.pkl"), "wb") as fp:
-    pickle.dump(val_1_data.paths, fp)
-with open(os.path.join(os.getcwd(), "Data", "all_data", "val_2_data_paths.pkl"), "wb") as fp:
-    pickle.dump(val_2_data.paths, fp)
+with open(join(os.getcwd(), "Data", args.dest_folder, "train_data_paths.pkl"), "wb") as fp:
+    pickle.dump(training_data, fp)
+with open(join(os.getcwd(), "Data", args.dest_folder, "val_1_data_paths.pkl"), "wb") as fp:
+    pickle.dump(val_1_data, fp)
+with open(join(os.getcwd(), "Data", args.dest_folder, "val_2_data_paths.pkl"), "wb") as fp:
+    pickle.dump(val_2_data, fp)
+
+if args.test_folder is not None:
+    # this folder name should indicate what test set we want (iid/ood/extreme/seasoanl)
+    test_set = args.test_folder.split('/')[-2]
+    
+    test_context_files = []
+    test_target_files = []
+    for path, subdirs, files in os.walk(os.getcwd() + args.test_folder):
+        for name in files:
+            if '.npz' in name:
+                full_name = join(path, name)
+                if 'context' in full_name:
+                    test_context_files.append(full_name)
+                elif 'target' in full_name:
+                    test_target_files.append(full_name)
+
+    # Sort file names just in case (so we glue together the right context & target)
+    test_context_files.sort()
+    test_target_files.sort()
+    with open(os.path.join(os.getcwd(), "Data", args.dest_folder, test_set+"_context_data_paths.pkl"), "wb") as fp:
+        pickle.dump(test_context_files, fp)
+    with open(os.path.join(os.getcwd(), "Data", args.dest_folder, test_set+"_target_data_paths.pkl"), "wb") as fp:
+        pickle.dump(test_target_files, fp)
 
 print("Done")
--- scripts/ensemble_score.py
+++ scripts/ensemble_score.py
@@ -1,38 +1,48 @@
 import numpy as np
-from itertools import product, chain, combinations
+from itertools import chain, combinations
 
-
-from os import listdir
+from datetime import datetime
+from os.path import join
 import os
-import pickle
 from numpy import genfromtxt
-from numpy.core.fromnumeric import mean, repeat
 
 scores = []
 names = []
 
-for path, subdirs, files in os.walk(os.path.join(os.getcwd(), "scripts", "Ensemble_score")):
+score_dir = "scripts/Ensemble_score"
+t = str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
+output_file = join(score_dir, "ensemble_scores_" + t + ".txt")
+
+with open(output_file, 'w') as f:
+    f.write("Evaluating ensemble at time " + t + "\n")
+
+# Collect all csv files (model scores)
+for path, subdirs, files in os.walk(join(os.getcwd(), score_dir)):
     for name in files:
-        names.append(name[7:-4])
-        scores.append(genfromtxt(os.path.join(os.getcwd(), "scripts", "Ensemble_score", name), delimiter=','))
+        if '.csv' in name:
+            names.append(name[7:-4])
+            scores.append(genfromtxt(os.path.join(os.getcwd(), score_dir, name), delimiter=','))
 
 nm = len(names)
 
 scr = np.nan_to_num(np.stack(scores, axis=0), nan=0)
-
+# Calculate the harmonic mean of ENS components
 def hm(scores):
     if np.min(scores) == 0:
         return 0
     else:
         return 4 / np.sum(1/scores)
 
+# Evaluate each model individually
 for i in range(nm):
     components = np.mean(scr[i], axis=0)
-    print(names[i])
-    print("    Old ENS " + str(components[-1]))
-    print("    Lazy ENS " + str(hm(components[:4])))
-    print("    MAD: {0} OLS: {1} EMD: {2} SSIM: {3}".format(components[0], components[2], components[3], components[1]))
+    with open(output_file, 'a') as f:
+        f.write(names[i] + "\n")
+        f.write("    Old ENS " + str(components[-1]) + "\n")
+        f.write("    Lazy ENS " + str(hm(components[:4])) + "\n")
+        f.write("    MAD: {0} OLS: {1} EMD: {2} SSIM: {3}".format(components[0], components[2], components[3], components[1]) + "\n")
 
+# Compute the ENS (+ component) for best prediction for each sample
 def en_score(s):
     best_score = np.zeros((s.shape[1], 6))
     for i in range(s.shape[1]):
@@ -40,29 +50,29 @@ def en_score(s):
         best_score[i, :5] = s[best,i,:]
         best_score[i, -1] = best
     means = np.mean(best_score[:,:-1], axis=0)
-    return means[-1], hm(means[:4]), means[:4]
-    
-print("Score of ensemble of ALL models")
-print(en_score(scr))
+    return means[-1], hm(means[:4]), means[[0,2,3,1]]
+
+with open(output_file, 'a') as f:
+    f.write("Score of ensemble of ALL models" + "\n")
+    f.write(str(en_score(scr)) + "\n")
 
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
+# Compute scores of each subset ensemble of models
 lst = list(powerset(list(range(nm))))[1+nm:]
 e_scores = []
 for l in lst:
     l=list(l)
-    print(l)
     e_scores.append(en_score(scr[l])[1])
-    print(e_scores[-1])
+    with open(output_file, 'a') as f:
+        f.write(str(l) + "\n")
+        f.write(str(e_scores[-1]) + "\n")
 
 e_scores, lst = zip(*sorted(zip(e_scores, lst)))
 for i in range(len(e_scores)):
-    print(str(lst[i]) + ": " + str(e_scores[i]))
-
-#for i in range(2,nm+1):
-
-
-#for i in ensemble:
+    with open(output_file, 'a') as f:
+        f.write(str(lst[i]) + ": " + str(e_scores[i]) + "\n")
+    #print(str(lst[i]) + ": " + str(e_scores[i]))
