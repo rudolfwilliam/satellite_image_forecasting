28b8f4c40ac2215672d1048e5e702c68db1fc83e
omrazCZ <oto.mraz@gmail.com>
2022-02-23 15:41:17 +0100

    Fix resuming training

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from os import listdir
+from pathlib import Path
 import json
 import wandb
 import random
@@ -22,8 +24,32 @@ def main():
         os.environ["WANDB_MODE"]="online"
     else:
         os.environ["WANDB_MODE"]="offline"
+    
+    if cfg_training["checkpoint"] is not None:
+        old_wandb_dir = str(Path(cfg_training["checkpoint"]).parents[2])
+        old_run_id = [f for f in listdir(old_wandb_dir) if 'run-' in f][0][4:-6]
 
-    wandb.init(entity="eth-ds-lab", project="Drought Impact Forecasting", config={"model_type":model_type,"training":cfg_training,"model":cfg_model})
+        wandb.init(entity="eth-ds-lab",
+                   project="Drought Impact Forecasting",
+                   config={"model_type":model_type,"training":cfg_training,"model":cfg_model},
+                   id=old_run_id,
+                   resume="must")
+
+        wandb_logger = WandbLogger(project='DS_Lab',
+                                   config={"model_type":model_type,"training":cfg_training,"model":cfg_model},
+                                   job_type='train',
+                                   offline=True,
+                                   id=old_run_id)
+
+    else:
+        wandb.init(entity="eth-ds-lab",
+                   project="Drought Impact Forecasting",
+                   config={"model_type":model_type,"training":cfg_training,"model":cfg_model})
+
+        wandb_logger = WandbLogger(project='DS_Lab',
+                                   config={"model_type":model_type,"training":cfg_training,"model":cfg_model},
+                                   job_type='train',
+                                   offline=True)
 
     # store the model name to wandb
     with open(os.path.join(wandb.run.dir, "run_name.txt"), 'w') as f:
@@ -33,21 +59,12 @@ def main():
             f.write("offline_run_" + str(datetime.now()))
 
     # setup model
-    if cfg_training["checkpoint"] is not None:
-        # Resume training from checkpoint
-        model = EN_model.load_from_checkpoint(cfg_training["checkpoint"])
-        model_type = model.model_type
-        cfg_model = model.cfg_model
-        cfg_training = model.cfg_training
-    else:
-        model = EN_model(model_type, cfg_model, cfg_training)
+    model = EN_model(model_type, cfg_model, cfg_training)
 
     with open(os.path.join(wandb.run.dir, "Training.json"), 'w') as fp:
         json.dump(cfg_training, fp)    
     with open(os.path.join(wandb.run.dir, model_type + ".json"), 'w') as fp:
         json.dump(cfg_model, fp)
-    
-    wandb_logger = WandbLogger(project='DS_Lab', config={"model_type":model_type,"training":cfg_training,"model":cfg_model}, job_type='train', offline=True)
     
     random.seed(cfg_training["seed"])
     pl.seed_everything(cfg_training["seed"], workers=True)
@@ -72,7 +89,7 @@ def main():
                                           save_top_k = -1,
                                           filename = 'model_{epoch:03d}')
 
-    # set up trainer
+    # set up trainer    
     trainer = Trainer(max_epochs=cfg_training["epochs"], 
                       logger=wandb_logger,
                       devices=cfg_training["devices"],
@@ -81,7 +98,10 @@ def main():
                       num_sanity_val_steps=1)
 
     # run training
-    trainer.fit(model, EN_dataset)
+    if cfg_training["checkpoint"] is None:
+        trainer.fit(model, EN_dataset)
+    else:
+        trainer.fit(model, datamodule=EN_dataset, ckpt_path=cfg_training["checkpoint"])
 
     if not cfg_training["offline"]:
         wandb.finish()
